c58f9ebbbdd5354391afd4a0a4b08184a909b6b3
ricky8221 <110708834+ricky8221@users.noreply.github.com>
2024-02-19 14:59:49 -0800

    rename function's name

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module NDE_backend
 
 go 1.21.6
 
-require github.com/ricky8221/NDE_DB v0.0.0-20240217044731-cd813018b351 // indirect
+require (
+	github.com/google/uuid v1.6.0 // indirect
+	github.com/ricky8221/NDE_DB v0.0.0-20240219225721-f8baffe7b24e // indirect
+)
--- main.go
+++ main.go
@@ -1,8 +1,14 @@
-// main.go
 package main
 
-import "github.com/ricky8221/NDE_DB"
+import (
+	"fmt"
+	sqlcFunc "github.com/ricky8221/NDE_DB/sqlc_func"
+)
 
 func main() {
-	NDE_DB.LogInfo("Hello Ricky")
+	res := sqlcFunc.CreateCompany{CompanyName: "ABC Company", CompanyContactName: "John Doe", CompanyContactNumber: "123-123-1231", Remark: "null"}
+
+	if res.CompanyName == "ABC Company" {
+		fmt.Print("create account successfully")
+	}
 }
