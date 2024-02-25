bcee611ed0c1dc336aa1031d0be01139b2ba55e4
ricky8221 <110708834+ricky8221@users.noreply.github.com>
2024-02-25 11:18:17 -0800

    successfully connect the sql function from private package

--- main.go
+++ main.go
@@ -1,14 +1,54 @@
 package main
 
 import (
-	"fmt"
-	sqlcFunc "github.com/ricky8221/NDE_DB/sqlc_func"
+	"context"
+	"database/sql"
+	"github.com/gin-gonic/gin"
+	_ "github.com/lib/pq"
+	"github.com/ricky8221/NDE_DB/sqlc_func"
+	"github.com/ricky8221/NDE_DB/util"
+	"log"
+	"net/http"
 )
 
 func main() {
-	res := sqlcFunc.CreateCompany{CompanyName: "ABC Company", CompanyContactName: "John Doe", CompanyContactNumber: "123-123-1231", Remark: "null"}
+	// Initialize the Gin engine.
+	r := gin.Default()
 
-	if res.CompanyName == "ABC Company" {
-		fmt.Print("create account successfully")
+	// DB connect string
+	connStr := util.GetConnStr()
+
+	// Open a database connection.
+	db, err := sql.Open("postgres", connStr)
+	if err != nil {
+		log.Fatal(err)
 	}
+	defer db.Close()
+
+	// Verify our connection is good.
+	err = db.Ping()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	r.POST("/createCompany", func(c *gin.Context) {
+		var req sqlc_func.CreateCompanyReq
+
+		// Bind the JSON to the struct
+		if err := c.ShouldBindJSON(&req); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+
+		company, err := sqlc_func.CreateCompany(context.Background(), db, req)
+		if err != nil {
+			// Handle the error appropriately
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to create company: " + err.Error()})
+			return
+		}
+
+		c.JSON(http.StatusOK, gin.H{"status": "success", "company": company})
+	})
+
+	r.Run()
 }
